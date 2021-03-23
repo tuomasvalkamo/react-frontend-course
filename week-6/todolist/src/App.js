47b8670a3ec60e199d82a1290d8d2bcfac0bbf25
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-03-24 01:43:33 +0200

    Added Material UI and date picker

--- week-6/todolist/src/App.js
+++ week-6/todolist/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
+
 import Todolist from './components/Todolist';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <h1>Simple Todolist</h1>
-      </div>
-      <Todolist />
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              TodoList
+          </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Todolist />
+      </MuiPickersUtilsProvider>
     </div>
   );
 }
--- week-6/todolist/src/components/Todolist.js
+++ week-6/todolist/src/components/Todolist.js
@@ -1,31 +1,51 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { DatePicker } from '@material-ui/pickers'
 
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 
 function Todolist() {
   const [todo, setTodo] = useState({ description: '', date: '', priority: '' });
+  const [date, setDate] = useState(new Date());
   const [todos, setTodos] = useState([]);
 
+  // Set todo state to reflect date picker default value
+  useEffect(() => {
+    setTodo({ ...todo, date: date.toLocaleString('fi-FI') })
+  }, [])
+
+  // Get reference to grid row
   const gridRef = useRef();
 
+  // Handle input change
   const inputChanged = (event) => {
     setTodo({ ...todo, [event.target.name]: event.target.value });
   }
 
+  // Handle date picker change
+  const handleDateChange = (event) => {
+    setDate(event);
+    setTodo({ ...todo, date: event.toLocaleString('fi-FI') })
+  }
+
+  // Add todo to todos
   const addTodo = () => {
     setTodos([...todos, todo]);
   }
 
+  // Delete todo from grid
   const deleteTodo = () => {
     if (gridRef.current.getSelectedNodes().length > 0) {
       setTodos(todos.filter((todo, index) => index !== gridRef.current.getSelectedNodes()[0].childIndex));
     } else {
-      alert('Valitse ensin poistettava rivi.');
+      alert('You must first choose a row to delete it.');
     }
   }
 
+  // Todo columns
   const columns = [
     {
       headerName: "Description",
@@ -53,11 +73,48 @@ function Todolist() {
 
   return (
     <div>
-      <input type="text" onChange={inputChanged} placeholder="Description" name="description" value={todo.description} />
-      <input type="text" onChange={inputChanged} placeholder="Date" name="date" value={todo.date} />
-      <input type="text" onChange={inputChanged} placeholder="Priority" name="priority" value={todo.priority} />
-      <button onClick={addTodo}>Add</button>
-      <button onClick={deleteTodo}>Delete</button>
+      <div style={{ marginTop: 20 }}>
+        <TextField
+          style={{ marginRight: 10 }}
+          label="Description"
+          name="description"
+          onChange={inputChanged}
+          value={todo.description}
+        />
+        <DatePicker
+          style={{ marginRight: 10 }}
+          name="date"
+          label="Date"
+          format="dd/MM/yyyy"
+          autoOk
+          value={date}
+          onChange={event => handleDateChange(event)}
+        />
+        <TextField
+          style={{ marginRight: 10 }}
+          label="Priority"
+          name="priority"
+          onChange={inputChanged}
+          value={todo.priority}
+        />
+
+        <Button
+          style={{ marginTop: 10, marginRight: 10 }}
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
+        >
+          Add
+      </Button>
+        <Button
+          style={{ marginTop: 10 }}
+          onClick={deleteTodo}
+          variant="contained"
+          color="secondary"
+        >
+          Delete
+      </Button>
+      </div>
 
       <div className="ag-theme-material" style={{ width: '90%', height: '700px', margin: 'auto' }}>
         <AgGridReact
